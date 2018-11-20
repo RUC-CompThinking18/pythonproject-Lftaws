365a040cd72181ee812a396b8d9e9c2764b659d5
Logan <loganf96@gmail.com>
2018-11-20 16:59:53 -0500

    Fixed sleep, hurt block
    Fixed the sleep function and added the hurt block so neglected stats can harm the player

--- Project/GamePlay.py
+++ Project/GamePlay.py
@@ -1,5 +1,6 @@
 import Profile
 
+
 def game():
     #stats imported for the player to be displayed
     #stats[Health,Wellness,Hunger,Thirst,Rest,Money]
@@ -52,21 +53,93 @@ def game():
     Money = 2.5
     float(Money)
 
-    #Setting in case they exceed 100%
-    if Health > 100:
-        Health = 100
-    if Wellness > 100:
-        Wellness = 100
-    if Hunger > 100:
-        Hunger = 100
-    if Thirst > 100:
-        Thirst = 100
-    if Rest > 100:
-        Rest = 100
+
     #A while loop that causes the player to select what they want to do
     #While action is blank, this will loop. If health is above 0, this will constantly loop
     action = ""
     while action == "":
+
+        #Setting in case they exceed 100%
+        if Health > 100:
+            Health = 100
+        if Wellness > 100:
+            Wellness = 100
+        if Hunger > 100:
+            Hunger = 100
+        if Thirst > 100:
+            Thirst = 100
+        if Rest > 100:
+            Rest = 100
+        #or 0%
+        if Health < 0:
+            Health = 0
+        if Wellness < 0:
+            Wellness = 0
+        if Hunger < 0:
+            Hunger = 0
+        if Thirst < 0:
+            Thirst = 0
+        if Rest < 0:
+            Rest = 0
+
+        #Block for harming the player for stat neglect.
+        #Wellness Block
+        if Wellness < 10:
+            Health = Health - 10
+        elif Wellness < 20:
+            Health = Health - 8
+        elif Wellness < 30:
+            Health = Health - 4
+        elif Wellness < 40:
+            Health = Health - 2
+        elif Wellness < 50:
+            Health = Health - 1
+
+        #Hunger Block
+        if Hunger < 10:
+            Wellness = Wellness - 5
+            Health = Health - 3
+        elif Hunger < 20:
+            Wellness = Wellness - 4
+            Health = Health - 2
+        elif Hunger < 30:
+            Wellness = Wellness - 3
+            Health = Health - 1
+        elif Hunger < 40:
+            Wellness = Wellness - 2
+        elif Hunger < 50:
+            Wellness = Wellness - 1
+
+        #Thirst Block
+        if Thirst < 10:
+            Wellness = Wellness - 5
+            Health = Health - 3
+        elif Thirst < 20:
+            Wellness = Wellness - 4
+            Health -2
+        elif Thirst < 30:
+            Wellness = Wellness - 3
+            Health = Health - 1
+        elif Thirst < 40:
+            Wellness = Wellness - 2
+        elif Thirst < 50:
+            Wellness = Wellness - 1
+
+        #Rest Block
+        if Rest == 0:
+            Wellness = Wellness - 5
+        elif Rest < 10:
+            Wellness = Wellness - 4
+        elif Rest < 20:
+            Wellness = Wellness - 3
+        elif Rest < 30:
+            Wellness = Wellness - 2
+        elif Rest < 40:
+            Wellness = Wellness - 1
+
+        if Health == 0:
+            return "You have died"
+
         #A display to show players stats and time
         print ("")
         print (int(Health), str(" Health"))
@@ -75,36 +148,46 @@ def game():
         print (int(Thirst), str(" Thirst"))
         print (int(Rest), str(" Rest"))
         print (str("$"), int(Money))
-        print ("It is " ,int(Time), "o'clock")
+        print ("It is " , int(Time) , "o'clock")
         print ("")
+
         #Asking player for input.
-        #As of Nov 15, there are no options for shopping or medicating, and answers need to be typed as shown
+
         action = raw_input("""What would you like to do? Beg , Shop , Sleep , Eat , Drink , Medicate\n\n""")
+
         #If action is to beg, will give money out and reduce stats.
         if action == "Beg" :
             Money = Money + .50
             Time = Time + 1
-            Hunger = Hunger - 5
-            Thirst = Thirst - 5
-            Rest = Rest - 8
+            Hunger = Hunger - 2
+            Thirst = Thirst - 3
+            Rest = Rest - 6
             #action = "" resets the loop and asks for prompt again
             action = ""
+
         #If player chooses to sleep, asks for how long. If its above 8, they get a full sleep.
         #Also reduces player stats based on how many hours they slept. They spend less energy, so lost stats are reduced.
         if action == "Sleep":
-            #Sleeping is bugged due to an error with adding the two integers (Time) and (HowMuchSlept)
             HowMuchSleep = raw_input("""How many hours would you like to sleep for?\n\n""")
-            Rest = HowMuchSleep * 10
-            Time = Time + HowMuchSleep
-            Hunger = Hunger - (3 * HowMuchSleep)
-            Thirst = Thirst - (5 * HowMuchSleep)
-            action = ""
+            HowMuchSleep = int(HowMuchSleep)
+            if HowMuchSleep < 8:
+                Rest = Rest + (HowMuchSleep * 10)
+                Time = Time + HowMuchSleep
+                Hunger = Hunger - (2 * HowMuchSleep)
+                Thirst = Thirst - (3 * HowMuchSleep)
+                action = ""
+            #Checking to see if the player sleeps for 8 or more hours
             if HowMuchSleep >= 8:
                 Rest = 100
-                Time = int(Time) + int(HowMuchSleep)
-                Hunger = Hunger - (3 * HowMuchSleep)
-                Thirst = Thirst - (5 * HowMuchSleep)
+                Time = Time + HowMuchSleep
+                Hunger = Hunger - (2 * HowMuchSleep)
+                Thirst = Thirst - (3 * HowMuchSleep)
+                HowMuchSleep = 0
                 action = ""
+            else:
+                print "That is not a valid input\n\n"
+                action = ""
+
 
         #If player chose to eat, they will be prompted to eat what in their inventory
         #This will be based on a list of items that can be edible.
@@ -113,6 +196,7 @@ def game():
             print()
             print()
             EatWhat = raw_input("What would you like to eat?\n\n")
+
             #Test block to add hunger when eating a hot dog, and if incorrect prompt is given, brings player back.
             if EatWhat == "Hot Dog":
                 Hunger = Hunger + 20
@@ -121,6 +205,7 @@ def game():
             else:
                 Print ("You decided to eat nothing.\n\n")
                 action = ""
+
         #Similar to eat block, but this is for drinking
         if action == "Drink":
             print Inventory
@@ -131,9 +216,11 @@ def game():
                 Thirst = Thirst + 20
                 print ("That quenched your thirst.\n\n")
                 action = ""
+            #If wrong input is put in, the player will drink nothing
             else:
                 print ("You decided to not drink anything\n\n")
                 action = ""
+
         if action == "Medicate":
             print Inventory
             print()
@@ -143,32 +230,6 @@ def game():
                 Health = Health + 15
                 print ("The pain is going away.\n\n")
                 action = ""
-        #Block for harming the player for stat neglect.
-        # def harm():
-         #    if Wellness < 50:
-          #     Health = Health - 1
-           # elif Wellness < 40:
-            #    Health = Health - 2
-            #elif Wellness < 30:
-             #   Health = Health - 4
-  #          elif Wellness < 20:
-   #             Health = Health - 8
-    #        elif Wellness < 10:
-     #           Health = Health - 10
-#
- #               if Hunger < 50:
-  #                  Wellness = Wellness - 1
-   #             elif Hunger < 40:
-    #                Wellness = Wellness - 2
-     #           elif Hunger < 30:
-      #              Wellness = Wellness - 3
-       #             Health = Health - 1
-        #        elif Hunger < 20:
-         #           Wellness = Wellness - 4
-          #          Health = Health - 2
-           #     elif Hunger < 10:
-            #        Wellness = Wellness - 5
-             #       Health = Health - 3
-              #  harm()
+
 #gameplay()
 game()
