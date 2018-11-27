116dc74a96e47d0182391733355bf3746eb9dda4
Logan <loganf96@gmail.com>
2018-11-27 00:36:40 -0500

    Removed old Profiles file, added readme, changed name of actual game, to the actual game name
    Removed a redundant, extra file which would not have provided use to the game. Changed the actual gameplay file name to be more clear to show what it is. Added a readme file.

--- Project/ThisIsLiving.py
+++ Project/ThisIsLiving.py
@@ -1,82 +1,251 @@
-#The start of the program Imports every profile from the profiles folder.
-from Profiles import profile
-Profile == open(Profiles)
-#A definition made for the player either creating a profile, or selecting one from the Profiles folder
-def ProfileSelect():
-    #This class holds the name of the profile/PLAYER, the DATA that is stored in their playthrough, and LOCATION on the map
-    #The if statement is to check to see if there are any other save files within the folder. If not, it makes the user create one
-    class Profile (player, Data, Location):
+def game():
+    #stats imported for the player to be displayed
+    #stats[Health,Wellness,Hunger,Thirst,Rest,Money]
+    stats = []
+    def before():
+        #A check to see if the player is JUST starting their playthrough
+        answer = raw_input (""" Welcome THIS IS LIVING? You will be playing as a homeless person, down on their luck in an unforgiving environment. You will have to manage your health, feed yourself, find water and shelter, and try to find money to get out of this situation. Things will be rough, difficult, and unfair. Life always is. On the streets of the city, anything goes, and anything can go wrong. You could even die. Please type I KNOW to begin. \n\n""")
+        while answer != ("I KNOW"):
+            answer = raw_input ("""I dont believe you understand the situation you currently find youself in. The city is full of hazards, and you must survive, and earn enough money to find a way out of this hell. You risk life and limb living like this, and theres only one way out. Type I KNOW when you are about to begin.\n\n""")
 
-        #Self commented notes: tests to see code working. Please Ignore
-        #str.player = ""
-        #Profiles = 0
-        #End of self note block
+        #Stats are set like this to start the game off with.
 
-        if Profiles == 0:
-            #Asks for the player's name, then saves it to the profiles folder ONLY if there are no profiles detected in the folder.
-            #(Self Note) change str.player to player.name for all instances
-            player.name = input("What is your name?")
-            player.name = Profile
-            output = Profile
-        #Else statement made to have the player pick a profile with raw_input
-        else:
-            print "Select your profile."
-            print Profiles
-            player.name == raw_input()
-            #Checks to see if the type between the input and available profiles is valid or not.
-            while player.name != Profiles:
-                print "That is not a valid profile"
-                player.name = raw_input ("Please select your profile")
-                print Profiles
-        print "Welcome "+ Player
+    before()
+#actual start of the game
+    #def gameplay():
+        #while Health != 0:
+        #Players inventory, items can be added or subtracted here.
+        #Inventories will be sorted into what the player can do with them. Such as eat, drink, or use.
+    Inventory = {
+    "Hot Dog" : 1,
+    "Water Bottle" : 1,
+    #Pain Killers have no function as of yet
+    "Pain Killers" : 1,
+    #Sleeping bag has no function as of yet
+    "Sleeping Bag" : 1
+    }
+    #Day and Time
+    Time = 0
+    # A function that holds how time is reset and what day it will be.
+    def TimeDay():
+        Day = ["Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"]
+        Time = 6
+        Day = [0]
+        if Time > 24:
+            Day = Day + 1
+            Time = 1
+    TimeDay()
 
-#This next section of code will dictate what will happen to the game. Either making a new one, continuing , or deleting the save
-GameState = raw_input("""What would you like to do? Start
-NEW GAME, CONTINUE Game, DELETE SAVE?
-Please type your answer""")
-#The while loop checks to see if the user input matches the available commands.
-    while GameState != "New Game" or != "Continue Game" or != "Delete Save":
-        GameState = raw_input("""That is not a valid command!
-        Please select either
-        New Game, Continue, or Delete Save""")
-#This if statement happens when the player selects Delete Save, it will, as stated, delete your save
-    if GameState = "Delete Save":
-        print "Are you sure you want to DELETE your save?""
-
-#Raw input is needed in order to delete the player, it imports profiles in
-#case it does not have the information, then can delete the players
-#profile, and then update the profiles folder
-
-        if GameState= "Yes" :
-            import Profiles(player)
-            remove(player)
-            print "Player has been lost"
-            output (profiles)
-            return
-        else:
-            GameState = raw_input("Please select either New Game, Continue Game, or Delete Save")
-        #Loops back to player input for GameState
-        while GameState != "Yes" or != "No" :
-            GameState = raw_input("""That is not a valid command
-            please select either
-            NEW GAME, CONTINUE, or DELETE SAVE""")
-    #If statements to determine how the data, player stats, and location will be treated.
-    #This if statement resets all data stored in the file.
-    if GameState == "New Game":
-        Stats=0
-        Data=0
-        Location=0
-        return Game(Player)
-    #This if statement simply calls the data without editing it.
-    if GameState == "Continue Game":
-        def Game(Stats,Data,Location)
+    #Stats are being set values and declared as ints.
+    Health = 75
+    int(Health)
+    Wellness = 75
+    int(Wellness)
+    Hunger = 50
+    int(Hunger)
+    Thirst = 50
+    int(Thirst)
+    Rest = 100
+    int(Rest)
+    Money = 2.5
+    float(Money)
 
 
-ProfileSelect()
+    #A while loop that causes the player to select what they want to do
+    #While action is blank, this will loop. If health is above 0, this will constantly loop
+    action = ""
+    while action == "":
 
-#Ignore this comment block, these are notes for myself.
-#gender = rawinput("Are you a boy or a girl?: ")
-#classes are defined as class variable()
-#then those classes will have to be defined. self.a self.b self.c
-#Ignore this. This block will be added later.
-#class Stats[Health,Hunger,Thirst,Rest,Wellness]
+        #Setting in case they exceed 100%
+        if Health > 100:
+            Health = 100
+        if Wellness > 100:
+            Wellness = 100
+        if Hunger > 100:
+            Hunger = 100
+        if Thirst > 100:
+            Thirst = 100
+        if Rest > 100:
+            Rest = 100
+        #or 0%
+        if Health < 0:
+            Health = 0
+        if Wellness < 0:
+            Wellness = 0
+        if Hunger < 0:
+            Hunger = 0
+        if Thirst < 0:
+            Thirst = 0
+        if Rest < 0:
+            Rest = 0
+
+        #Block for harming the player for stat neglect.
+        #Wellness Block
+        if Wellness < 10:
+            Health = Health - 10
+            print ("\nYou feel like you are dying...\n")
+        elif Wellness < 20:
+            Health = Health - 8
+            print ("\nYou are starting to feel absolutely terrible.\nPlease take care of yourself or you will die.\n")
+        elif Wellness < 30:
+            Health = Health - 4
+            print ("\nYou are feeling very unwell.\nYou must take care of your needs.\n")
+        elif Wellness < 40:
+            Health = Health - 2
+            print ("\nYou are starting to feel unwell\n")
+        elif Wellness < 50:
+            Health = Health - 1
+            print ("\nYou're not feeling so good.\n")
+
+        #Hunger Block
+        if Hunger < 10:
+            Wellness = Wellness - 5
+            Health = Health - 3
+            print ("\nYou are at risk of fading away.\n YOU MUST EAT.\n")
+        elif Hunger < 20:
+            Wellness = Wellness - 4
+            Health = Health - 2
+            print ("\nYour body aches and you begin to feel weak.\nYou have to find food.\n")
+        elif Hunger < 30:
+            Wellness = Wellness - 3
+            Health = Health - 1
+            print ("\nYour stomach growls in dire need of food.\n")
+        elif Hunger < 40:
+            Wellness = Wellness - 2
+            print ("\nYou are feeling hungry.\n")
+        elif Hunger < 50:
+            Wellness = Wellness - 1
+            print ("\nYou are feeling a bit peckish\n")
+
+        #Thirst Block
+        if Thirst < 10:
+            Wellness = Wellness - 5
+            Health = Health - 3
+            print ("\nYour throat and body begin to shut down.\nYou NEED a drink\n")
+        elif Thirst < 20:
+            Wellness = Wellness - 4
+            Health -2
+            print ("\nYou are dying of dehydration\n")
+        elif Thirst < 30:
+            Wellness = Wellness - 3
+            Health = Health - 1
+            print ("You really should get something to drink\n")
+        elif Thirst < 40:
+            Wellness = Wellness - 2
+            print ("\nYou are thirsty.\n")
+        elif Thirst < 50:
+            Wellness = Wellness - 1
+            print ("\nYou feel a bit parched.\n")
+
+        #Rest Block
+        if Rest == 0:
+            Wellness = Wellness - 5
+            print ("\nYou are on the brink of collapse.\n")
+        elif Rest < 10:
+            Wellness = Wellness - 4
+            print ("\nYou struggle to stay awake.\n")
+        elif Rest < 20:
+            Wellness = Wellness - 3
+            print ("\nYou feel exhausted.\n")
+        elif Rest < 30:
+            Wellness = Wellness - 2
+            print ("\nYou should probably get some sleep.\n")
+        elif Rest < 40:
+            Wellness = Wellness - 1
+            print ("\nYou feel a bit tired.\n")
+        if Health == 0:
+            return "You have died"
+
+        #A display to show players stats and time
+        print ("")
+        print (int(Health), str(" Health"))
+        print (int(Wellness), str(" Wellness"))
+        print (int(Hunger), str(" Hunger"))
+        print (int(Thirst), str(" Thirst"))
+        print (int(Rest), str(" Rest"))
+        print (str("$"), int(Money))
+        print ("It is " , int(Time) , "o'clock")
+        print ("")
+
+        #Asking player for input.
+
+        action = raw_input("""What would you like to do? Beg , Shop , Sleep , Eat , Drink , Medicate\n\n""")
+
+        #If action is to beg, will give money out and reduce stats.
+        if action == "Beg" :
+            Money = Money + .50
+            Time = Time + 1
+            Hunger = Hunger - 2
+            Thirst = Thirst - 3
+            Rest = Rest - 6
+            #action = "" resets the loop and asks for prompt again
+            action = ""
+
+        #If player chooses to sleep, asks for how long. If its above 8, they get a full sleep.
+        #Also reduces player stats based on how many hours they slept. They spend less energy, so lost stats are reduced.
+        if action == "Sleep":
+            HowMuchSleep = raw_input("""How many hours would you like to sleep for?\n\n""")
+            HowMuchSleep = int(HowMuchSleep)
+            if HowMuchSleep < 8:
+                Rest = Rest + (HowMuchSleep * 10)
+                Time = Time + HowMuchSleep
+                Hunger = Hunger - (2 * HowMuchSleep)
+                Thirst = Thirst - (3 * HowMuchSleep)
+                action = ""
+            #Checking to see if the player sleeps for 8 or more hours
+            if HowMuchSleep >= 8:
+                Rest = 100
+                Time = Time + HowMuchSleep
+                Hunger = Hunger - (2 * HowMuchSleep)
+                Thirst = Thirst - (3 * HowMuchSleep)
+                HowMuchSleep = 0
+                action = ""
+            else:
+                print "That is not a valid input\n\n"
+                action = ""
+
+
+        #If player chose to eat, they will be prompted to eat what in their inventory
+        #This will be based on a list of items that can be edible.
+        if action  == "Eat":
+            print Inventory
+            print()
+            print()
+            EatWhat = raw_input("What would you like to eat?\n\n")
+
+            #Test block to add hunger when eating a hot dog, and if incorrect prompt is given, brings player back.
+            if EatWhat == "Hot Dog":
+                Hunger = Hunger + 20
+                print ("Delicious\n\n")
+                action = ""
+            else:
+                Print ("You decided to eat nothing.\n\n")
+                action = ""
+
+        #Similar to eat block, but this is for drinking
+        if action == "Drink":
+            print Inventory
+            print()
+            print()
+            DrinkWhat = raw_input("What would you like to drink?\n\n")
+            if DrinkWhat == "Water Bottle":
+                Thirst = Thirst + 20
+                print ("That quenched your thirst.\n\n")
+                action = ""
+            #If wrong input is put in, the player will drink nothing
+            else:
+                print ("You decided to not drink anything\n\n")
+                action = ""
+
+        if action == "Medicate":
+            print Inventory
+            print()
+            print()
+            MedWhat = raw_input("What will you use?\n\n")
+            if MedWhat == "Pain Killers":
+                Health = Health + 15
+                print ("The pain is going away.\n\n")
+                action = ""
+
+#gameplay()
+game()
---
+++ Project/readme.txt
@@ -0,0 +1,5 @@
+Hello. You are currently looking at the read me text file for a student game named (This Is Living?)
+
+This is a python 2.7.6 project for the Rutgers University Computational Thinking course.
+
+There is only one primary file for this program, which is called ThisIsLiving.py, the others are leftover coding review files.
